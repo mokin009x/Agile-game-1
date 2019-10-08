a18ea948610bd777269e390d75ccc7a1df559484
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-10-08 15:42:56 +0200

    made Scene for enemy ai testing. made enemy ai that pick random possition on navmesh and walks there. the ai can also shoot the player.

--- Assets/Jaimy/Scripts/BasicBullet.cs
+++ Assets/Jaimy/Scripts/BasicBullet.cs
@@ -12,7 +12,7 @@ public class BasicBullet : MonoBehaviour
     public int speed;
     public int bounceAmount;
 
-    public LayerMask collisionMask;
+    public LayerMask wallMask;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -29,17 +29,28 @@ public class BasicBullet : MonoBehaviour
     {
         Ray ray = new Ray(transform.position,transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray,out hit, Vector3.Distance(ray.origin,ray.direction),collisionMask))
+
+        if (bounceAmount > 0)
         {
-            rb.velocity = Vector3.zero;
-            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
-            float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(0,rot,0);
-            rb.AddForce(transform.forward * speed);
-            Debug.DrawRay(ray.origin, ray.direction,Color.red, 20);
-
-
+            if (coll.gameObject.CompareTag("Player"))
+            {
+             Destroy(this.gameObject);   
+            }
+            
+            if (Physics.Raycast(ray,out hit, Vector3.Distance(ray.origin,ray.direction),wallMask))
+            {
+                rb.velocity = Vector3.zero;
+                Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+                float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0,rot,0);
+                rb.AddForce(transform.forward * speed);
+                Debug.DrawRay(ray.origin, ray.direction,Color.red, Mathf.Infinity);
+                bounceAmount--;
+            }
+            
         }
+        
+       
     }
 
     // Update is called once per frame
---
+++ Assets/Jaimy/Scripts/EnemyAi.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class EnemyAi : MonoBehaviour
+{
+    public enum fireModes
+    {
+        Automatic = 1
+    }
+    public GameObject cannon;
+    public GameObject bullet;
+    public GameObject target;
+    public float distance;
+    public float detectionRange;
+    public bool canFire;
+    public fireModes fireMode;
+    public float walkRange;
+    public bool walking;
+    public NavMeshAgent instanceAgent;
+    public Vector3 newWalkPos;
+
+
+    private void Awake()
+    {
+        instanceAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result) {
+        for (int i = 0; i < 30; i++) {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instanceAgent.destination = transform.position;
+        walkRange = 10.0f;
+        canFire = true;
+        walking = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance <= detectionRange)
+        {
+           cannon.transform.LookAt(target.transform, Vector3.up);
+           FireCannon();
+        }
+        Wander();
+    }
+
+    public void Wander()
+    {
+        if (instanceAgent.remainingDistance <= 0.5f)
+        {
+            if (RandomPoint(transform.position, walkRange, out newWalkPos))
+            {
+                instanceAgent.destination = newWalkPos;
+            }
+        }
+    }
+
+    public void FireCannon()
+    {
+        if (fireMode == fireModes.Automatic)
+        {
+            if (canFire == true)
+            {
+                Instantiate(bullet, cannon.transform.position, cannon.transform.rotation);
+                StartCoroutine(FireDelayIE(0.2f));  
+            }
+        }
+    }
+
+    public IEnumerator FireDelayIE(float delay)
+    {
+         canFire = false;
+         yield return new WaitForSeconds(delay);
+         canFire = true;
+    }
+
+}
