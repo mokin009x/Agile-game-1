839964845056219c7180dcbdf58756ad46e7a68e
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-10-11 09:04:12 +0200

    forgot to stage these

--- Assets/Jaimy/Scripts/BasicBullet.cs
+++ Assets/Jaimy/Scripts/BasicBullet.cs
@@ -13,6 +13,7 @@ public class BasicBullet : MonoBehaviour
     public int bounceAmount;
 
     public LayerMask wallMask;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,41 +22,40 @@ public class BasicBullet : MonoBehaviour
 
     void Start()
     {
-        
         rb.AddForce(transform.forward * speed);
     }
 
     private void OnCollisionEnter(Collision coll)
     {
-        Ray ray = new Ray(transform.position,transform.forward);
+        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
         if (bounceAmount > 0)
         {
-            if (coll.gameObject.CompareTag("Player"))
+            if (coll.gameObject.layer != LayerMask.NameToLayer("Walls"))
             {
-             Destroy(this.gameObject);   
+                Destroy(this.gameObject);
             }
-            
-            if (Physics.Raycast(ray,out hit, Vector3.Distance(ray.origin,ray.direction),wallMask))
+
+            if (Physics.Raycast(ray, out hit, Vector3.Distance(ray.origin, ray.direction), wallMask))
             {
                 rb.velocity = Vector3.zero;
                 Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
                 float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
-                transform.eulerAngles = new Vector3(0,rot,0);
+                transform.eulerAngles = new Vector3(0, rot, 0);
                 rb.AddForce(transform.forward * speed);
-                Debug.DrawRay(ray.origin, ray.direction,Color.red, Mathf.Infinity);
+                Debug.DrawRay(ray.origin, ray.direction, Color.red, Mathf.Infinity);
                 bounceAmount--;
             }
-            
         }
-        
-       
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
     }
 }
